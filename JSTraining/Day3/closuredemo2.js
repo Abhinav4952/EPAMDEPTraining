69568ba638f7bb96b6825123a2c3d63f746ce6da
Abhinav Veeramosu <Abhinav_Veeramosu@epam.com>
2020-03-16 15:22:12 +0530

    Used ES6 features

--- JSTraining/Day3/closuredemo2.js
+++ JSTraining/Day3/closuredemo2.js
@@ -5,14 +5,13 @@ We want to create a function, which returns an array of functions, which return 
 function createFunctions(n) {
   var callbacks = [];
 
-  for (var i=0; i<n; i++) {
-    (function () { 
-            var temp = i; 
-            var res = function () { return temp; };
-            callbacks.push(res);
-    }());
-  }
- 
-  return callbacks;
+  for (var i in n) {
+    (()=> {          
+      var temp = i; 
+      var res = ()=> temp ;
+      callbacks.push(res);
+    })();
 }
 
+return callbacks;
+}
